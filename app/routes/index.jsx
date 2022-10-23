367d9d356a1adf7929aea58681d9dc50dc1bf851
Your Name <hiwa101@hotmail.com>
2022-10-23 10:16:43 +0200

    first commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div className="h-screen w-full bg-slate-600">
+      <h1 className="font-bold text-5xl text-blue-400">Welcome to Remix</h1>
+      
+    </div>
+  );
+}
